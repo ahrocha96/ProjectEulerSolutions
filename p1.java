0c632a8264bb435e1b8b98c099ab412675af5f23
Andrew Rocha <ahrocha96@gmail.com>
2018-10-09 13:36:56 -0400

    Problem 1 solution

---
+++ p1.java
@@ -0,0 +1,16 @@
+
+public class p1 {
+	public static void main(String args[]) {
+		int sum = 0;
+		for (int i = 0; i < 1000; i = i + 3) {
+			sum = sum + i;
+		}
+		for (int i = 0; i < 1000; i = i + 5) {
+			sum = sum + i;
+			if (i % 15 == 0) {
+				sum = sum - i;
+			}
+		}
+		System.out.println(sum);
+	}
+}
