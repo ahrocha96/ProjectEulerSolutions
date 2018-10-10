c2977cfabf2cf6dfbdbc717a995e9326ab59a8e4
Andrew Rocha <ahrocha96@gmail.com>
2018-10-10 10:25:33 -0400

    Optimized solution

--- p3.java
+++ p3.java
@@ -3,34 +3,35 @@ public class p3 {
 	public static void main(String args[]) {
 		long n = 600851475143L;
 		long largestPrimeFactor = 0;
-		for(long i = 1; i<n; i++){
+		int counter = 0;
+		for (long i = 1; i <= n; i=i+2) {
 			System.out.println(largestPrimeFactor);
-			if(isFactor(i) && isPrime(i)){
+			counter = counter+1;
+			System.out.println(counter);
+			if (isFactor(i, n) && isPrime(i)) {
+				n = n / i;
 				largestPrimeFactor = i;
 			}
 		}
 		System.out.println(largestPrimeFactor);
 	}
 
-	public static boolean isFactor(long x) {
-		long n = 600851475143L;
-		System.out.println(x);
+	public static boolean isFactor(long x, long n) {
 		if (n % x == 0)
 			return true;
 		else
 			return false;
 
 	}
-	
-	public static boolean isPrime(long x){
+
+	public static boolean isPrime(long x) {
 		int factorCounter = 0;
-		for(long i = 1; i<=x; i++){
-			if(x%i == 0){
+		for (long i = 1; i <= x; i++) {
+			if (x % i == 0) {
 				factorCounter = factorCounter + 1;
 			}
 		}
-		System.out.println(factorCounter);
-		if (factorCounter>2)
+		if (factorCounter > 2)
 			return false;
 		else
 			return true;
